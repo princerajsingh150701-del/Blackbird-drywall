f80b91bae27bd86dd100639690bdd49a8f96f2d0
princerajsingh150701-del <princerajsingh150701@gmail.com>
2025-10-17 17:27:21 -0700

    Update ugc.js

---
+++ ugc.js
@@ -0,0 +1,20 @@
+gsap.from(".page1", {
+    duration: 1,
+    
+    scale: 0.5,
+    opacity: 0,
+    ease: "power2.out",
+    delay: 0.5
+  });
+
+  gsap.from(".page2 .page2left", {
+    x: 600, // Animation properties
+    duration: 5,
+    scrollTrigger: {
+      trigger: ".page2", // Element that triggers the animation
+      start: "top 0%", // When the top of the trigger hits the center of the viewport
+      end: "bottom 50%", // When the bottom of the trigger hits the top of the viewport
+      scrub: true, // Link animation progress to scroll position
+      markers: true // Optional: Display visual markers for start/end points
+    }
+  });
